b33b04927f8061f9ef5ed62b1b0841513cd2e3ab
Oren Leavitt <oleavitt@ix.netcom.com>
2025-03-22 01:16:16 -0500

    Add swiftlint to project build phase

--- FetchTakeHome/FetchTakeHome/RecipesList/RecipesListView.swift
+++ FetchTakeHome/FetchTakeHome/RecipesList/RecipesListView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct RecipesListView: View {
     @StateObject var viewModel = RecipesListViewModel()
-    
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
