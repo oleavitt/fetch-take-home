d94490fec5ebe61e9986fc107f1ecf21edd64e1b
Oren Leavitt <oleavitt@ix.netcom.com>
2025-03-22 00:29:42 -0500

    Initial skeleton project

---
+++ FetchTakeHome/FetchTakeHome/FetchTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeApp.swift
+//  FetchTakeHome
+//
+//  Created by Oren Leavitt on 3/22/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
